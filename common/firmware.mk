f46673d463e1c4364d69118c317c561cf68c1af5
sgnanase <sundar.gnanasekaran@intel.com>
2017-02-20 16:00:51 +0530

    Summary : Avoid copy of unused firmware binaries.
    Fix  : Added support to copy only required list of (intel specific)
           firmware bins files/folders/patterns to system/vendor/firmware.
    
    Jira : https://01.org/jira/browse/AIA-114
    
    Test : Build boots to home screen. Wifi, & speaker
           working fine.
    
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- common/firmware.mk
+++ common/firmware.mk
@@ -14,7 +14,26 @@
 # limitations under the License.
 #
 
-LOCAL_FIRMWARES ?= $(filter-out $(FIRMWARE_FILTERS),$(subst ./,,$(shell cd $(FIRMWARES_DIR) && find . -type f)))
+## List of Firmware bin files to be copied
+
+LOCAL_FIRMWARE_SRC := \
+    i6050-fw-usb-1.5.sbcf \
+    i2400m-fw-usb-1.4.sbcf \
+    i2400m-fw-usb-1.5.sbcf
+
+## List of complete Firmware folders to be copied
+
+LOCAL_FIRMWARE_DIR := \
+    intel \
+    i915
+
+## List of matching patterns of Firmware bins to be copied
+
+LOCAL_FIRMWARE_PATTERN := \
+    iwlwifi
+
+LOCAL_FIRMWARE_SRC += $(foreach f,$(LOCAL_FIRMWARE_PATTERN),$(shell cd $(FIRMWARES_DIR) && find . -iname "*$(f)*" -type f ))
+LOCAL_FIRMWARE_SRC += $(foreach f,$(LOCAL_FIRMWARE_DIR),$(shell cd $(FIRMWARES_DIR) && find $(f) -type f) )
 
 PRODUCT_COPY_FILES := \
-    $(foreach f,$(LOCAL_FIRMWARES),$(FIRMWARES_DIR)/$(f):system/vendor/firmware/$(f))
+    $(foreach f,$(LOCAL_FIRMWARE_SRC),$(FIRMWARES_DIR)/$(f):system/vendor/firmware/$(f))
