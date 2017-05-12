9aefbbd6eaf0396836314e339bbd288ef06c1dca
Kishore Kadiyala <kishore.kadiyala@intel.com>
2017-05-12 20:12:27 +0530

    Fix: factory partition entry added in android_ia
    Factory partition entry is added in gpt.ini instead
    maintain it in mixins/groups/android_ia as done for other
    partitions.
    
    Jira : AIA-397
    Test : The build should generate a factory.img
    	The CI test bench flashing should work
    
    Change-Id: I754f2e3e6f426190daae87b4978fe18c0fe74d1d
    Signed-off-by: Kishore Kadiyala <kishore.kadiyala@intel.com>

--- androidia_64/BoardConfig.mk
+++ androidia_64/BoardConfig.mk
@@ -213,7 +213,7 @@ endif
 # Source: device/intel/mixins/groups/factory-partition/true/BoardConfig.mk
 ##############################################################
 BOARD_FACTORYIMAGE_PARTITION_SIZE := 10485760
-BOARD_SEPOLICY_DIRS += device/intel/sepolicy/factory-partition
+BOARD_SEPOLICY_DIRS += device/intel/android_ia/sepolicy/factory-partition
 BOARD_SEPOLICY_M4DEFS += module_factory_partition=true
 ##############################################################
 # Source: device/intel/mixins/groups/debug-phonedoctor/true/BoardConfig.mk
