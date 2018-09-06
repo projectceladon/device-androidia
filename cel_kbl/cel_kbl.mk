e40e098b76d311c71dfe1935cb8291c144d36582
Yan, WalterX <walterx.yan@intel.com>
2018-09-06 15:26:31 +0800

    Make sure GAS overlay take prcedence than Car product overlay.
    Tracked-On: OAM-68502
    Signed-off-by: Yan, WalterX <walterx.yan@intel.com>

--- cel_kbl/cel_kbl.mk
+++ cel_kbl/cel_kbl.mk
@@ -1,6 +1,14 @@
 ENABLE_NATIVEBRIDGE_64BIT := true
 
 $(call inherit-product,$(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Include GAS application. 
+# Must put this calling before device.mk to make sure GAS overlay take
+# precedence than Car product overlay
+FLAG_GAS_MINIMAL := true
+FLAG_GAS_AVAILABLE := true
+$(call inherit-product-if-exists, vendor/google/gas/products/gas.mk)
+
 $(call inherit-product,device/intel/project-celadon/cel_kbl/device.mk)
 
 # Overrides
