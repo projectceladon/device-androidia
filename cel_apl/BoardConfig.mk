f819ab06de20bd19dcacb738e25fd84dd257fb39
Meng, KangX <kangx.meng@intel.com>
2018-11-01 10:53:18 +0800

    [MIXINUP] Change the vendor partition size from 1500MB to 1800MB. In order to the kernel add debug info.
    Tracked-On: OAM-71088
    Signed-off-by: Meng, KangX <kangx.meng@intel.com>

--- cel_apl/BoardConfig.mk
+++ cel_apl/BoardConfig.mk
@@ -149,7 +149,7 @@ AB_OTA_PARTITIONS += vbmeta
 # Those 3 lines are required to enable vendor image generation.
 # Remove them if vendor partition is not used.
 TARGET_COPY_OUT_VENDOR := vendor
-BOARD_VENDORIMAGE_PARTITION_SIZE := 1572864000
+BOARD_VENDORIMAGE_PARTITION_SIZE := 1887436800
 ifeq ($(SPARSE_IMG),true)
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 else
