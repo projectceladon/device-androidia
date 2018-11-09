67491608970a6ec1caecea1601033dc485e61571
Meng Xianglin <xianglinx.meng@intel.com>
2018-11-09 17:47:05 +0800

    [MIXINUP] Enable early mount in Celadon
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-69202
    Signed-off-by: Meng Xianglin <xianglinx.meng@intel.com>

--- celadon/BoardConfig.mk
+++ celadon/BoardConfig.mk
@@ -467,4 +467,10 @@ BOARD_SEPOLICY_DIRS += device/intel/project-celadon/sepolicy/memtrack
 # can't use := here, as PRODUCT_OUT is not defined yet
 GPTIMAGE_BIN = $(PRODUCT_OUT)/$(TARGET_PRODUCT)_gptimage.img
 CRAFFIMAGE_BIN = $(PRODUCT_OUT)/$(TARGET_PRODUCT)_gptimage.craff
+##############################################################
+# Source: device/intel/mixins/groups/firststage-mount/true/BoardConfig.mk
+##############################################################
+BOARD_FIRSTSTAGE_MOUNT_ENABLE := true
+BOARD_KERNEL_CMDLINE += androidboot.android_dt_dir=/sys/bus/platform/devices/ANDR0001:00/properties/android/
+FIRSTSTAGE_MOUNT_SSDT = $(PRODUCT_OUT)/firststage-mount.aml
 # ------------------ END MIX-IN DEFINITIONS ------------------
