467fd3f5dbff2034e16ddae1535e67c619b84d0d
Ming Tan <ming.tan@intel.com>
2018-12-17 16:52:43 +0800

    [DO NOT MERGE] Mixinup fo disable update_verifier.
    Signed-off-by: Ming Tan <ming.tan@intel.com>

--- cel_apl/device.mk
+++ cel_apl/device.mk
@@ -322,7 +322,6 @@ endif
 ##############################################################
 PRODUCT_PACKAGES += \
     update_engine \
-    update_verifier \
     update_engine_sideload \
     libavb \
     bootctrl.avb \
--- celadon/device.mk
+++ celadon/device.mk
@@ -321,7 +321,6 @@ endif
 ##############################################################
 PRODUCT_PACKAGES += \
     update_engine \
-    update_verifier \
     update_engine_sideload \
     libavb \
     bootctrl.avb \
