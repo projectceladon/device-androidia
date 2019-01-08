347af5fd700c7bdd54e44f68c735cf9605d7c817
rnaidu <ramya.v.naidu@intel.com>
2019-01-08 02:07:04 -0500

    Creating new lunch target for cel_kbl
    Tracked-On: OAM-74962
    Signed-off-by: rnaidu <ramya.v.naidu@intel.com>

---
+++ cel_kbl/cel_kbl.mk
@@ -0,0 +1,21 @@
+ENABLE_NATIVEBRIDGE_64BIT := true
+
+$(call inherit-product,$(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Include GAS application. 
+# Must put this calling before device.mk to make sure GAS overlay take
+# precedence than Car product overlay
+FLAG_GAS_MINIMAL := true
+FLAG_GAS_AVAILABLE := true
+$(call inherit-product-if-exists, vendor/google/gas/products/gas.mk)
+
+$(call inherit-product,device/intel/project-celadon/cel_kbl/device.mk)
+
+# Overrides
+PRODUCT_NAME := cel_kbl
+PRODUCT_BRAND := cel_kbl
+PRODUCT_DEVICE := cel_kbl
+PRODUCT_MODEL := Generic cel_kbl
+PRODUCT_MANUFACTURER := Intel
+
+PRODUCT_SHIPPING_API_LEVEL := 25
