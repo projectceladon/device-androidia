466c8b0db871c3f6a59eaad140f5a4fe21c7cc92
buildslave <ctbbot@intel.com>
2019-02-12 16:22:37 +0000

    mixin updates
    bluetooth: Add changes for vendor complaint
    - Define macro to use correct libbt-vendor
    - Remove not used file to store bt paired information
    - Move loading of bluetooth modules from post-fs phase
    to boot phase.
    iwlwifi: Add changes for treble complaint
    IWifi VTS test cases are not getting executed
    due to p2p interface is not set properly.
    Following changes are done to fix the issue:
    - Set wifi.direct.interface to correct value
    - Add p2p configuration file
    - Remove not needed configuration file
    - Add android.hardware.wifi.direct.xml for supporting
    p2p feature
    - Move loading of wifi drivers from post-fs to boot
    phase.
    
    Tracked-On: OAM-75987
    Tracked-On: OAM-75990
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/191
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/192
    Signed-off-by: Amrita Raju <amrita.raju@intel.com>
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- cel_kbl/BoardConfig.mk
+++ cel_kbl/BoardConfig.mk
@@ -120,7 +120,7 @@ BOARD_SEPOLICY_DIRS += device/intel/project-celadon/sepolicy/kernel
 ##############################################################
 # Source: device/intel/mixins/groups/bluetooth/btusb/BoardConfig.mk
 ##############################################################
-BOARD_HAVE_BLUETOOTH_LINUX_PRI := true
+BOARD_HAVE_BLUETOOTH_INTEL_ICNV := true
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-bt-pan
 BOARD_SEPOLICY_DIRS += device/intel/project-celadon/sepolicy/bluetooth/common
 
