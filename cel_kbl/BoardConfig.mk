4d330c090c81ed0d79710ef97ad90b7c4e4d2eb4
buildslave <ctbbot@intel.com>
2019-03-15 09:39:32 +0000

    mixin updates
    Kernel : Support USB serial console in APL NUC
    1.Enable the USB serial console.
    2.Set apl_nuc lunch target use USB serial console,and the others use the default ttyS0.
    3.Only support FTDI USB serial convertor now.
    add serialport group for config the serial port console
    set the "console=" parameter in the boot command line
    
    Tracked-On: OAM-76637
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/226
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia/pull/336
    Signed-off-by: Yanhongx.Zhou <yanhongx.zhou@intel.com>

--- cel_kbl/BoardConfig.mk
+++ cel_kbl/BoardConfig.mk
@@ -112,7 +112,7 @@ TARGET_PRELINK_MODULE := false
 TARGET_NO_KERNEL ?= false
 
 KERNEL_LOGLEVEL ?= 3
-SERIAL_PARAMETER ?= console=tty0 console=ttyS0,115200n8
+SERIAL_PARAMETER ?= console=tty0
 
 
 BOARD_KERNEL_CMDLINE += androidboot.hardware=$(TARGET_PRODUCT) firmware_class.path=/vendor/firmware loglevel=$(KERNEL_LOGLEVEL) loop.max_part=7
@@ -516,4 +516,10 @@ BOARD_SEPOLICY_DIRS += device/intel/project-celadon/sepolicy/power
 # Source: device/intel/mixins/groups/default-drm/true/BoardConfig.mk
 ##############################################################
 BOARD_SEPOLICY_DIRS += device/intel/project-celadon/sepolicy/drm-default
+##############################################################
+# Source: device/intel/mixins/groups/serialport/ttyS0/BoardConfig.mk
+##############################################################
+ifneq ($(TARGET_BUILD_VARIANT),user)
+BOARD_KERNEL_CMDLINE += console=ttyS0,115200n8
+endif
 # ------------------ END MIX-IN DEFINITIONS ------------------
