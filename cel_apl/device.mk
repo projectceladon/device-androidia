7c302ab4df07ea2684c203756d475bbcabf9afcf
buildslave <ctbbot@intel.com>
2019-03-21 06:11:26 +0000

    mixin updates
    debug-logs: add the vendor to vendor_default_ prop
    the property under /vendor should be marked as vendor_default_prop
    for vendor components to access
    so add the vendor for the property
    
    Tracked-On: OAM-77013
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/250
    Signed-off-by: chenxia1 <xiao.x.chen@intel.com>

--- cel_apl/device.mk
+++ cel_apl/device.mk
@@ -599,11 +599,11 @@ PRODUCT_PACKAGES += \
 endif
 
 ifeq ($(MIXIN_DEBUG_LOGS),true)
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.service.default_logfs=apklogfs
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.intel.logger=/system/bin/logcat
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += logd.kernel.raw_message=False
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.intel.logger.rot_cnt=20
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.intel.logger.rot_size=5000
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.vendor.service.default_logfs=apklogfs
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.vendor.intel.logger=/system/bin/logcat
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += vendor.logd.kernel.raw_message=False
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.vendor.intel.logger.rot_cnt=20
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.vendor.intel.logger.rot_size=5000
 BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/debug-logs
 BOARD_SEPOLICY_M4DEFS += module_debug_logs=true
 endif
