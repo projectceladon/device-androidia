2f2f6f461704580e7ec211c419cff57263aca64a
buildslave <ctbbot@intel.com>
2020-05-11 07:30:51 +0000

    mixin updates
    Mixins chnages to enable WiFi HAL
    
    Tracked-On: OAM-90807
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/651
    Signed-off-by: Raveendra Babu Chennakesavulu

--- cic/BoardConfig.mk
+++ cic/BoardConfig.mk
@@ -162,7 +162,9 @@ NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 ##############################################################
 # Source: device/intel/mixins/groups/wlan/mac80211_hwsim/BoardConfig.mk
 ##############################################################
+BOARD_WLAN_DEVICE           := emulator
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_simulated
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 WIFI_DRIVER_FW_PATH_PARAM   := "/dev/null"
 WIFI_DRIVER_FW_PATH_STA     := "/dev/null"
