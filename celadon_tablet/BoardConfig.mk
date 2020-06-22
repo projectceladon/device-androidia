e75ed2f34a8c3ecd6664dae441e9a767a3dee4b5
buildslave <ctbbot@intel.com>
2020-06-22 05:16:26 +0000

    mixin updates
    Fix for usb ethernet device check
    REVERT ME: Pick the corresponding mixer path
    Changes to pick the corresponding mixer path based on build flag
    Set dsp_driver parameter to force legacy HDA driver
    The current guest kernel sometimes picks the sof audio driver
    for loading the HDA/HDMI audio devices. But since sof driver is
    not yet enabled for android, adding dsp_driver param to force
    the Legacy HDA Driver to be picked
    Fix for HDMI audio playback on EHL platform
    HDMI port on EHL platform is listed as device 7, where as,
    the default device for HDMI was set to 3, hence,
    adding an additional check for EHL platform.
    
    Tracked-On: OAM-91411
    Tracked-On: OAM-91412
    Tracked-On: OAM-91467
    Tracked-On: OAM-91476
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/753
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/754
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/756
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/757
    Signed-off-by: Raveendra Babu Chennakesavulu
    Signed-off-by: akodanka <anoob.anto.kodankandath@intel.com>
    Signed-off-by: gkdeepa <g.k.deepa@intel.com>

--- celadon_tablet/BoardConfig.mk
+++ celadon_tablet/BoardConfig.mk
@@ -245,6 +245,11 @@ BOARD_KERNEL_CMDLINE += \
 BOARD_KERNEL_CMDLINE += \
       snd-hda-intel.model=dell-headset-multi
 
+ifeq ($(BASE_YOCTO_KERNEL), true)
+BOARD_KERNEL_CMDLINE += \
+      snd-intel-dspcfg.dsp_driver=1
+endif
+
 BOARD_SEPOLICY_M4DEFS += module_kernel=true
 BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/kernel
 ##############################################################
@@ -277,6 +282,8 @@ INTEL_AUDIO_HAL := audio
 else
 INTEL_AUDIO_HAL := stub
 endif
+
+BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/audio
 
 # Use XML audio policy configuration file
 USE_XML_AUDIO_POLICY_CONF := 1
