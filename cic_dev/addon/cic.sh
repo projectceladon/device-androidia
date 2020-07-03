9a90f32b2758f4cb1a8939c3a867e81728614c92
buildslave <ctbbot@intel.com>
2020-07-03 13:21:52 +0000

    mixin updates
    Remove '-e' option for cent os.
    -e currently doesn't work well with cent os.
    Network and inputs are non-functional.
    Will be reverted back once this issue is resolved.
    Enable adaptive install script for cic_dev
    
    Tracked-On: OAM-91417
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/766
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia/pull/911
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- cic_dev/addon/cic.sh
+++ cic_dev/addon/cic.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-snd_load=`lsmod | grep -i snd_dummy`
+function start_until_user_login {
+local pipe=workdir/ipc/multiuserpipe
 
-if [[ -z $snd_load ]]; then
-  /sbin/modprobe snd-dummy
+if [[ ! -p $pipe ]]; then
+    mkfifo $pipe
+    chmod 777 $pipe
 fi
 
+while true
+do
+    if read line <$pipe; then
+        if [ -z "$line" ]; then
+            break
+        fi
+        if [ "$CURRENT_USER" != "$line" ]; then
+            echo "start AIC for user: $line"
+            CURRENT_USER=$line
+            /bin/bash aic start -u $line
+        fi
+    fi
+done
+}
+
+if [ "$1" == "start" ] && [ -d "workdir/data-multi-user/data" ]; then
+    start_until_user_login
+else
 /bin/bash aic $1
+fi
