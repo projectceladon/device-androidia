13d3d5a0632da1158618ec30d3f5e940d089bd7a
Tanuj Tekriwal <tanuj.tekriwal@intel.com>
2021-04-20 12:50:14 +0530

    Mixin Updates for partner_modules
    Tracked-On: OAM-96832
    Signed-off-by: Salini Venate <svenate@intel.com>

--- caas/device.mk
+++ caas/device.mk
@@ -550,6 +550,7 @@ PRODUCT_COPY_FILES += \
 FLAG_GMS_AVAILABLE ?= true
 ifeq ($(FLAG_GMS_AVAILABLE),true)
 $(call inherit-product-if-exists, vendor/google/gms/products/gms.mk)
+$(call inherit-product, vendor/partner_modules/build/mainline_modules.mk)
 endif
 ##############################################################
 # Source: device/intel/mixins/groups/debug-tools/true/product.mk
