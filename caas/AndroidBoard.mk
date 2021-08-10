08e547cb1714d8e6d1d5143228b688474cfb478f
buildslave <ctbbot@intel.com>
2021-08-10 18:30:45 +0000

    mixin updates
    Add lts2020-yocto kernel support
    - Added kernel config files
    - Updated hwcomposer and gralloc properties for i915 frame buffer
    Add lts2020-yocto kernel support
    [media] set hardware codec when using new kernel
    It will check /proc/fb, if it is i915drmfb and inteldrmfb ,
    , codec will be hardware, otherwise codec will be software.
    Add new case i915
    
    Tracked-On: OAM-97996
    Tracked-On: OAM-98129
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1152
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1179
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia/pull/1203
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
    Signed-off-by: Kanli Hu <kanli.hu@intel.com>

--- caas/AndroidBoard.mk
+++ caas/AndroidBoard.mk
@@ -283,6 +283,9 @@ ifeq ($(BASE_CHROMIUM_KERNEL), true)
 else ifeq ($(BASE_YOCTO_KERNEL), true)
   LOCAL_KERNEL_SRC := kernel/lts2019-yocto
   KERNEL_CONFIG_PATH := $(TARGET_DEVICE_DIR)/config-lts/lts2019-yocto
+else ifeq ($(BASE_LTS2020_YOCTO_KERNEL), true)
+  LOCAL_KERNEL_SRC := kernel/lts2020-yocto
+  KERNEL_CONFIG_PATH := $(TARGET_DEVICE_DIR)/config-lts/lts2020-yocto
 else
   LOCAL_KERNEL_SRC := kernel/lts2019-chromium
   EXT_MODULES := 
