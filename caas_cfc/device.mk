475d0578820c5fe94122e68201c1cb0c7e9252b3
buildslave <ctbbot@intel.com>
2021-09-08 14:19:37 +0000

    mixin updates
    Include ClipboardAgent system app to caas_cfc
    This is to support clipboard copy/paste feature
    for caas_cfc target.
    
    Tracked-On: OAM-99249
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1206
    Signed-off-by: ahs <amrita.h.s@intel.com>

--- caas_cfc/device.mk
+++ caas_cfc/device.mk
@@ -262,6 +262,8 @@ PRODUCT_PACKAGES += \
     ServiceAgent \
     pm_agent_client
 
+PRODUCT_PACKAGES += ClipboardAgent
+
 PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl \
                     android.hardware.keymaster@3.0-service \
                     android.hardware.usb@1.0-impl \
