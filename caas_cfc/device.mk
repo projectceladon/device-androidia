ddd4cdc4d2c052cc146318a050caea5618e381ce
buildslave <ctbbot@intel.com>
2022-02-17 21:19:38 +0000

    mixin updates
    Change ro.enable.native.bridge.exec to ro.vendor.enable.native.bridge.exec
    
    Tracked-On: OAM-100984
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1330
    Signed-off-by: tangwei <weix.tang@intel.com>

--- caas_cfc/device.mk
+++ caas_cfc/device.mk
@@ -784,7 +784,7 @@ AUTO_IN += $(TARGET_DEVICE_DIR)/extra_files/sensors/auto_hal.in
 TARGET_SUPPORTS_64_BIT_APPS := true
 
 PRODUCT_PACKAGES += libhoudini houdini
-PRODUCT_PROPERTY_OVERRIDES += ro.dalvik.vm.isa.arm=x86 ro.enable.native.bridge.exec=1
+PRODUCT_PROPERTY_OVERRIDES += ro.dalvik.vm.isa.arm=x86 ro.vendor.enable.native.bridge.exec=1
 
 ENABLE_NATIVEBRIDGE_64BIT := false
 ifeq ($(BOARD_USE_64BIT_USERSPACE),true)
@@ -796,7 +796,7 @@ else
 endif
 ifeq ($(ENABLE_NATIVEBRIDGE_64BIT),true)
   PRODUCT_PACKAGES += houdini64
-  PRODUCT_PROPERTY_OVERRIDES += ro.dalvik.vm.isa.arm64=x86_64 ro.enable.native.bridge.exec64=1
+  PRODUCT_PROPERTY_OVERRIDES += ro.dalvik.vm.isa.arm64=x86_64 ro.vendor.enable.native.bridge.exec64=1
 endif
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.dalvik.vm.native.bridge=libhoudini.so
 ##############################################################
