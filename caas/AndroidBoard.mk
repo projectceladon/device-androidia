b82c4da30ffce14c4cfa180b542637527bb6e8e0
buildslave <ctbbot@intel.com>
2022-02-18 16:43:00 +0000

    mixin updates
    Enable virtual ab ota on caas
    Add support for virtual ab ota
    1. Virtual a/b ota has dependences on dynamic partitions,
    so put the related configuration to dynamic partition
    group.
    2. rm vendor_boot_prebuilt target, it's not used currently,
    and it also will introduce a build failure after the Virtual
    a/b comperssion ota is enabled, since it will remove the
    dirs/files generated by the virtual a/b makefile.
    
    Tracked-On: OAM-100724
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1313
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia/pull/1348
    Signed-off-by: ji, zhenlong z <zhenlong.z.ji@intel.com>

--- caas/AndroidBoard.mk
+++ caas/AndroidBoard.mk
@@ -603,22 +603,6 @@ $(RECOVERY_VENDOR_BINARIES):  $(RECOVERY_VENDOR_LINKS)
 ALL_DEFAULT_INSTALLED_MODULES += \
        $(RECOVERY_VENDOR_BINARIES)
 ##############################################################
-# Source: device/intel/mixins/groups/vendor-boot/true/AndroidBoard.mk
-##############################################################
-BOARD_PREBUILT_VENDOR_BOOT_DIR := $(TARGET_DEVICE_DIR)/vendor_boot-ramdisk
-INTERNAL_VENDOR_RAMDISK_TARGET := $(call intermediates-dir-for,PACKAGING,vendor_boot)/vendor_ramdisk.cpio.gz
-
-.PHONY: vendor_boot_prebuilt
-vendor_boot_prebuilt:
-	$(hide) rm -rf $(TARGET_VENDOR_RAMDISK_OUT)/*
-	$(hide) mkdir -p $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk
-	$(hide) if [ -d "$(BOARD_PREBUILT_VENDOR_BOOT_DIR)" ]; then \
-		$(ACP) -r $(BOARD_PREBUILT_VENDOR_BOOT_DIR)/* $(TARGET_VENDOR_RAMDISK_OUT)/; \
-	fi
-	$(ACP) $(TARGET_DEVICE_DIR)/fstab $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk/fstab.$(TARGET_PRODUCT)
-
-$(INTERNAL_VENDOR_RAMDISK_TARGET): vendor_boot_prebuilt
-##############################################################
 # Source: device/intel/mixins/groups/acpio-partition/true/AndroidBoard.mk
 ##############################################################
 ACPIO_OUT := $(PRODUCT_OUT)/acpio
