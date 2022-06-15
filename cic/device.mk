50a56aec5303193ea98d717ef7be0a87caf191b9
buildslave <ctbbot@intel.com>
2022-06-15 02:53:45 +0000

    mixin updates
    Add impl for Trusty
    Add android.hardware.keymaster@3.0-impl for software level trusty.
    
    Tracked-On: OAM-102364
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1393
    Signed-off-by: Cui, Yuxin <yuxin.cui@intel.com>

--- cic/device.mk
+++ cic/device.mk
@@ -131,6 +131,7 @@ PRODUCT_COPY_FILES += \
 ##############################################################
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service.software \
+    android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 ##############################################################
 # Source: device/intel/mixins/groups/bluetooth/default/product.mk
