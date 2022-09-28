3127e2ef4f10ff94e12169e57e070a1205b9dd8b
buildslave <ctbbot@intel.com>
2022-09-28 19:39:53 +0000

    mixin updates
    Fix CtsDeqpTestCases dEQP-VK failure
    dEQP-VK.glsl.builtin_var.*
    dEQP-VK.multiview.* test failure fix
    Configure rro overlay for connectivity to fix CTS issue
    Keepalive packet offloading is enabled by default. CTS test cases
    are failing as the keepalive offloading event is not received.
    As Keepalive is not supported, configure the rro overlay for
    ServiceConnectivityResource to disable the
    networkSupportedKeepaliveCount to fix the Cts Net TC module issue.
    
    Tracked-On: OAM-103877
    Tracked-On: OAM-104109
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1475
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1486
    Signed-off-by: gollarx <ratnakumarix.golla@intel.com>
    Signed-off-by: vdanix <vishwanathx.dani@intel.com>

--- caas_cfc/device.mk
+++ caas_cfc/device.mk
@@ -100,7 +100,8 @@ PRODUCT_PACKAGES += \
     wpa_cli \
     iw \
     TetheringConfigOverlay \
-    TetheringConfigOverlayGsi
+    TetheringConfigOverlayGsi \
+    ServiceConnectivityResourcesConfigOverlay
 
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
