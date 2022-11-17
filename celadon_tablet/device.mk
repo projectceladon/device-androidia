e36c769542843417a4cc284646f913b41b75fd2c
buildslave <ctbbot@intel.com>
2022-11-17 09:10:56 +0000

    mixin updates
    Migrate Bluetooth Audio HAL to AIDL
    From Android T onwards, its mandatory to use
    AIDL for Bluetooth Audio HAL. So migrating it
    from HIDL to AIDL.
    Verified the music streaming and logcat logs
    for AIDL.
    
    Tracked-On: OAM-104808
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1534
    Signed-off-by: Gowtham Anandha Babu <gowtham.anandha.babu@intel.com>

--- celadon_tablet/device.mk
+++ celadon_tablet/device.mk
@@ -171,7 +171,7 @@ PRODUCT_PROPERTY_OVERRIDES += bluetooth.rfkill=1
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service.vbt \
     libbt-vendor \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
 
 PRODUCT_PACKAGE_OVERLAYS += $(INTEL_PATH_COMMON)/bluetooth/overlay-car
 PRODUCT_PACKAGE_OVERLAYS += $(INTEL_PATH_COMMON)/bluetooth/intel/car/overlay
