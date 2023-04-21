0ade6a3ffa57db7490ea30a0c1ce3d168565e4b5
buildslave <ctbbot@intel.com>
2023-04-21 21:35:24 +0000

    mixin updates
    Enable ai-dispatcher raw tensor service with vsock
    - add remote inference in civ host as a service.
    - add scripts to setup and start raw tensor service
    on host. To setup ai-dispatcher, while doing host setup use
    "sudo -E ./scripts/setup_host.sh -i <device>"
    note: CPU is only enabled for now, GPU works in SRIOV
    - add vsock:2:50059 as default port for remote inference
    Add flag to enable vsock use for remote inference via nnhal
    set flag for vsock-remote-infer to true which sets vsocket port
    for caas nnhal remote inference
    
    Tracked-On: OAM-108910
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1614
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia/pull/1630
    Signed-off-by: Ratnesh Kumar Rai <ratnesh.kumar.rai@intel.com>

--- celadon_ivi/BoardConfig.mk
+++ celadon_ivi/BoardConfig.mk
@@ -702,4 +702,8 @@ TARGET_CPU_ABI_LIST_32_BIT += $(NB_ABI_LIST_32_BIT)
 TARGET_CPU_ABI_LIST_64_BIT += $(NB_ABI_LIST_64_BIT)
 
 BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/houdini
+##############################################################
+# Source: device/intel/mixins/groups/neuralnetworks/true/BoardConfig.mk
+##############################################################
+BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/neuralnetworks
 # ------------------ END MIX-IN DEFINITIONS ------------------
