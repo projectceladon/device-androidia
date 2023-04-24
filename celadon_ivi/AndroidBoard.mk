b30cae7adffc91955b630794fba33a9739c20f24
Wang, Liang <liang.wang@intel.com>
2023-04-24 22:43:51 +0800

    Add docker support
    Added docker runtime binaries and configurations files
    which provide running docker containers features for Android.
    
    Tracked-On: OAM-112319
    Signed-off-by: Lin, Ziniu <ziniu.lin@intel.com>
    Signed-off-by: Wang, Liang <liang.wang@intel.com>

--- celadon_ivi/AndroidBoard.mk
+++ celadon_ivi/AndroidBoard.mk
@@ -972,4 +972,22 @@ $(LOCAL_BUILT_MODULE): $(LOCAL_SRC)
 # Source: device/intel/mixins/groups/suspend/auto/AndroidBoard.mk
 ##############################################################
 AUTO_IN += $(TARGET_DEVICE_DIR)/extra_files/suspend/auto_hal.in
+##############################################################
+# Source: device/intel/mixins/groups/docker/true/AndroidBoard.mk
+##############################################################
+DOCKERD_ENV_RESOLV_CONF := $(PRODUCT_OUT)/system/etc/resolv.conf
+$(DOCKERD_ENV_RESOLV_CONF):
+	@ln -sf /data/vendor/docker/etc/resolv.conf $(PRODUCT_OUT)/system/etc/resolv.conf
+$(PRODUCT_OUT)/system.img:  $(DOCKERD_ENV_RESOLV_CONF)
+
+LIC_IMAGE_SOURCE := $(PRODUCT_OUT)/vendor/etc/docker/gamecore.tar
+$(LIC_IMAGE_SOURCE):
+	@mkdir -p $(PRODUCT_OUT)/vendor/etc/docker
+ifneq ($(wildcard $(TOP)/vendor/intel/weston-in-docker/gamecore),)
+	@tar --exclude .git -cf $(PRODUCT_OUT)/vendor/etc/docker/gamecore.tar -C $(TOP)/vendor/intel/weston-in-docker/gamecore .
+endif
+ifneq ($(wildcard $(TOP)/vendor/intel/weston-in-docker/aicore),)
+	@tar --exclude .git -cf $(PRODUCT_OUT)/vendor/etc/docker/aicore.tar -C $(TOP)/vendor/intel/weston-in-docker/aicore .
+endif
+$(PRODUCT_OUT)/vendor.img: $(LIC_IMAGE_SOURCE)
 # ------------------ END MIX-IN DEFINITIONS ------------------
