3fd691bc0b7e85ac8f1dd0585a4406162a1a49d1
buildslave <ctbbot@intel.com>
2023-07-26 12:49:38 +0000

    mixin updates
    CtsOsTestCases failed due to Missing SoC Properties.
    Following CTS test android.os.cts.BuildTest#testBuildConstants
    is failing due to missing SoC properties. A12 onwards, Below
    SoC properties need to define to pass CTS and cts-on-gsi test-:
    ro.soc.manufacturer
    ro.soc.model
    
    Tracked-On: OAM-111307
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1705
    Signed-off-by: Ankit Agarwal <ankit.agarwal@intel.com>
    Signed-off-by: Panda,SushreeX <sushreex.panda@intel.com>

--- caas/device.mk
+++ caas/device.mk
@@ -93,6 +93,8 @@ PRODUCT_COPY_FILES += system/extras/checkpoint_gc/checkpoint_gc.sh:recovery/root
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 KERNELFLINGER_SUPPORT_KEYBOX_PROVISION := true
+
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/extra_files/boot-arch/set_soc_prop.sh:vendor/bin/set_soc_prop.sh
 ##############################################################
 # Source: device/intel/mixins/groups/wlan/iwlwifi/product.mk
 ##############################################################
