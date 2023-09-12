2b4f3c75c89665b03270e78d1ac3448aeeebdf83
swaroopb <swaroop.balan@intel.com>
2023-09-12 12:49:18 +0530

    Adding new Lunch target for Development Purpose
    Tracked-On: OAM-112188
    Signed-off-by: swaroopb <swaroop.balan@intel.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,16 +16,10 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/celadon_ivi/celadon_ivi.mk \
-    $(LOCAL_DIR)/celadon_tablet/celadon_tablet.mk \
-    $(LOCAL_DIR)/caas/caas.mk \
-    $(LOCAL_DIR)/caas_dev/caas_dev.mk \
-    $(LOCAL_DIR)/cic/cic.mk \
-	$(LOCAL_DIR)/caas_cfc/caas_cfc.mk
+    $(LOCAL_DIR)/apollo_ivi/apollo_ivi.mk \
+    $(LOCAL_DIR)/blizzard_ivi/blizzard_ivi.mk
 
 COMMON_LUNCH_CHOICES += \
     celadon_ivi-userdebug \
-    celadon_tablet-userdebug \
-    caas-userdebug \
-    caas_dev-userdebug \
-    cic-userdebug \
-	caas_cfc-userdebug
+    apollo_ivi-userdebug \
+    blizzard_ivi-userdebug
