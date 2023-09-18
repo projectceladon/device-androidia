756ba4713adda1baa223747488efa5bb3130268a
buildslave <ctbbot@intel.com>
2023-09-18 06:19:01 +0000

    mixin updates
    Add ACRN hypervisor support
    Add ACRN for TEE VM support
    Enable acrn hypervisor in mixins.spec
    Enable acrn for Android and OPTEE support
    Use sw vulkan driver in VIRTIO-3D mode
    As virtio-gpu does not support hardware vulkan driver, updating the
    property to pastel so software vulkan driver is used.
    
    Tracked-On: OAM-111883
    Tracked-On: OAM-112144
    Tracked-On: OAM-112235
    Mixin-Reviewed-On: https://github.com/intel-innersource/os.android.bsp.androidia-apollo/pull/1
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1726
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1737
    Signed-off-by: Chen, Gang G <gang.g.chen@intel.com>
    Signed-off-by: chenyanxzhu <chenyanx.zhu@intel.com>

--- celadon_ivi/AndroidBoard.mk
+++ celadon_ivi/AndroidBoard.mk
@@ -172,8 +172,7 @@ endif
 else # BOOTLOADER_SLOT == false
 	$(hide) mkdir -p $(efi_root)/EFI/INTEL/
 endif # BOOTLOADER_SLOT
-	$(hide) $(ACP) $(PRODUCT_OUT)/sbl_os $(efi_root)/boot
-	$(hide) $(ACP) $(PRODUCT_OUT)/sbl_fb $(efi_root)/boot
+	$(hide) $(ACP) $(PRODUCT_OUT)/sbl_bm $(efi_root)/boot/sbl_os
 	$(hide) $(ACP) $(TOP)/$(PREBUILT_KERNELFLINGER) $(efi_root)/EFI/BOOT/bootx64.efi
 	$(hide) $(ACP) $(TOP)/$(PREBUILT_KERNELFLINGER) $(efi_root)/EFI/BOOT/bootia32.efi
 	$(hide) (cd $(efi_root) && zip -qry ../$(notdir $@) .)
@@ -200,8 +199,7 @@ $(hide)mmd -i $(BOARD_BOOTLOADER_VAR_IMG) ::capsules;
 $(hide)mmd -i $(BOARD_BOOTLOADER_VAR_IMG) ::EFI;
 $(hide)mmd -i $(BOARD_BOOTLOADER_VAR_IMG) ::EFI/BOOT;
 $(hide)mmd -i $(BOARD_BOOTLOADER_VAR_IMG) ::boot;
-$(hide)mcopy -Q -i $(BOARD_BOOTLOADER_VAR_IMG) $(PRODUCT_OUT)/sbl_os ::boot/sbl_os;
-$(hide)mcopy -Q -i $(BOARD_BOOTLOADER_VAR_IMG) $(PRODUCT_OUT)/sbl_fb ::boot/sbl_fb;
+$(hide)mcopy -Q -i $(BOARD_BOOTLOADER_VAR_IMG) $(PRODUCT_OUT)/sbl_bm ::boot/sbl_os;
 $(hide)mcopy -Q -i $(BOARD_BOOTLOADER_VAR_IMG) $(TOP)/$(PREBUILT_KERNELFLINGER) ::EFI/BOOT/bootx64.efi;
 cp $(BOARD_BOOTLOADER_VAR_IMG) $(BOARD_BOOTLOADER_DEFAULT_IMG)
 cp $(BOARD_BOOTLOADER_VAR_IMG) $(bootloader_bin)
