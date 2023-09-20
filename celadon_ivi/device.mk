2ab7ab848b8f69fc350015dfe570834118159486
Lin, Ziniu <ziniu.lin@intel.com>
2023-09-20 08:22:15 +0000

    Add docker support to Celadon
    This commit adds android docker support
    to Celadon.
    
    Tracked-On: OAM-112319
    Signed-off-by: Lin, Ziniu <ziniu.lin@intel.com>

--- celadon_ivi/device.mk
+++ celadon_ivi/device.mk
@@ -889,21 +889,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/extra_files/docker/containerd-shim-runc-v2:/vendor/bin/containerd-shim-runc-v2 \
     $(LOCAL_PATH)/extra_files/docker/docker:/vendor/bin/docker \
     $(LOCAL_PATH)/extra_files/docker/dockerd:/vendor/bin/dockerd \
+    $(LOCAL_PATH)/extra_files/docker/dockerd-dev:/vendor/bin/dockerd-dev \
     $(LOCAL_PATH)/extra_files/docker/runc:/vendor/bin/runc \
     $(LOCAL_PATH)/extra_files/docker/99-ignore-keyboard.rules:/vendor/etc/docker/config/99-ignore-keyboard.rules \
     $(LOCAL_PATH)/extra_files/docker/99-ignore-mouse.rules:/vendor/etc/docker/config/99-ignore-mouse.rules \
     $(LOCAL_PATH)/extra_files/docker/config.toml:/vendor/etc/docker/etc/containerd/config.toml \
     $(LOCAL_PATH)/extra_files/docker/daemon.json:/vendor/etc/docker/etc/docker/daemon.json
 
-PRODUCT_PACKAGES += sumClientInAndroid \
-    multiplyServiceInAndroid \
-    multiplyClientInAndroid \
-    subtractServiceApplicationInAndroid \
-    SharedBufferServiceInAndroid \
-    SharedBufferClientInAndroid
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.nn.hal.grpc_socket_path=/data/vendor/neuralnetworks/ai.socket
 ##############################################################
 # Source: device/intel/mixins/groups/debug-unresponsive/default/product.mk
 ##############################################################
